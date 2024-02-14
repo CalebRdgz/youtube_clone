4422815e9a1713aaf8f36bb56c91b8f2cde75094
CalebRdgz <calebrodgz@gmail.com>
2024-02-14 01:17:47 -0600

    Feed now shows videos from RapidAPI! Videos component takes in a prop 'videos' which has it's item and index mapped through the videos in the API, each box has its key as the current index, and renders videocard or channelcard component depending on if item id is pointing to videoId or channelId respectively. VideoCard component takes in a prop 'video' with it's id and snippet, takes in the video details like the title, creator, and thumbnail. ChannelCard component takes in the channelDetail prop which takes the channel details form the API. Future-proofing the subscribers count under the creator name.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './'
 
 const Videos = ({ videos }) => {
-    console.log(videos);
   return (
-    <div>Videos</div>
+    <Stack direction={'row'} flexWrap={'wrap'} justifyContent={'start'} gap={2}>
+        {/* map through our videos: */}
+        {videos.map((item, index) => (
+            // for each of the videos in the videos array render a video card inside Box container:
+            <Box key={index}>
+              {/* category tabs could show a channel profile, or just videos: */}
+              {/* if the current item has an id that points to a videoId, render the VideoCard component */}
+              {item.id.videoId && <VideoCard video={item} />}
+              {/* if the current item has an id that points to a channelId, render the ChannelCard component */}
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+        ))}
+    </Stack>
   )
 }
 
